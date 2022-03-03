b09e4278d49676ca7176b341fb104a1225e9629b
federicocecconi <fedececco9.github@manyme.com>
2022-03-03 17:03:48 +0100

    Update database.js

--- server/database.js
+++ server/database.js
@@ -4,13 +4,6 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
 
-//create connection 
-// const db = mysql.createPool({
-//     host: 'eu-cdbr-west-02.cleardb.net',
-//     user: 'b7a5f66721a6b6',
-//     password: '660b4226',
-//     database: 'heroku_27e06831b1f3ac2',
-// })
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
